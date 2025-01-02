47c52afe3c3c47125298eeddb3cbaf37e02a5a15
Uch-Mengly <mengly_uch@student.tuxglobalinstitute.edu.kh>
2025-01-02 10:52:36 +0700

    deploy

--- .output/server/chunks/ContactForm-styles.4a1dfbce.mjs
+++
@@ -1,6 +0,0 @@
-const ContactForm_vue_vue_type_style_index_0_scoped_d6bd6079_lang = "form[data-v-d6bd6079]{--tw-text-opacity:1;color:rgb(96 123 150/var(--tw-text-opacity));font-family:Fira Code Retina}input[data-v-d6bd6079]{background-color:#011221;border:2px solid #1e2d3d;border-radius:7px}input[data-v-d6bd6079]:-webkit-autofill,select[data-v-d6bd6079]:-webkit-autofill,textarea[data-v-d6bd6079]:-webkit-autofill{-webkit-text-fill-color:#bebebe;border:2px solid #607b96}#message-input[data-v-d6bd6079]{background-color:#011221;border:2px solid #1e2d3d;border-radius:7px;height:150px;padding:10px;resize:none}#submit-button[data-v-d6bd6079]{background-color:#1e2d3d;border-radius:7px;cursor:pointer;margin-top:20px}#submit-button[data-v-d6bd6079]:hover{background-color:#263b50}#message-input[data-v-d6bd6079]:focus,input[data-v-d6bd6079]:focus{border:2px solid #607b96;box-shadow:0 0 0 2px #607b9669;outline:none}#contact-form[data-v-d6bd6079]{max-width:370px;width:100%}";
-
-const ContactFormStyles_4a1dfbce = [ContactForm_vue_vue_type_style_index_0_scoped_d6bd6079_lang];
-
-export { ContactFormStyles_4a1dfbce as default };
-//# sourceMappingURL=ContactForm-styles.4a1dfbce.mjs.map
--- .output/server/chunks/close.19fba679.mjs
+++
@@ -1,5 +0,0 @@
-const _imports_0 = "" + globalThis.__publicAssetsURL("icons/arrow.svg");
-const _imports_1 = "" + globalThis.__publicAssetsURL("icons/close.svg");
-
-export { _imports_0 as _, _imports_1 as a };
-//# sourceMappingURL=close.19fba679.mjs.map
